c9a99b85b8d40bd4eaa871cf71662c01b7c923dc
2oublePeace <vova.emiryan@yandex.ru>
2023-10-12 00:35:43 +0400

    Созданы dto DiskRequest и DiskResponse

---
+++ src/main/kotlin/com/emiryanvl/diskaccounting/dto/request/DiskRequest.kt
@@ -0,0 +1,6 @@
+package com.emiryanvl.diskaccounting.dto.request
+
+class DiskRequest (
+    var title: String,
+    var isRented: Boolean = false
+)
---
+++ src/main/kotlin/com/emiryanvl/diskaccounting/dto/response/DiskResponse.kt
@@ -0,0 +1,6 @@
+package com.emiryanvl.diskaccounting.dto.response
+
+class DiskResponse (
+    var title: String,
+    var isRented: Boolean
+)
