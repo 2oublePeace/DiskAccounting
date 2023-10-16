be591ea560a7770c4b4fcec14fa7068b08707c85
2oublePeace <vova.emiryan@yandex.ru>
2023-10-16 15:24:35 +0400

    Добавлен модуль со шлюзом

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	kotlin("jvm") version "1.8.22"
 	kotlin("plugin.spring") version "1.8.22" apply false
-	kotlin("plugin.jpa") version "1.8.22" apply false
 	id("org.springframework.boot") version "3.1.4" apply false
 	id("io.spring.dependency-management") version "1.1.3" apply false
 }
-
-group = "com.emiryanvl"
-version = "0.0.1-SNAPSHOT"
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_17
@@ -27,7 +23,6 @@ allprojects {
 subprojects {
 	apply(plugin = "org.jetbrains.kotlin.jvm")
 	apply(plugin = "org.jetbrains.kotlin.plugin.spring")
-	apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
 	apply(plugin = "org.springframework.boot")
 	apply(plugin = "io.spring.dependency-management")
 
@@ -36,14 +31,6 @@ subprojects {
 		implementation("org.jetbrains.kotlin:kotlin-reflect")
 		testImplementation("org.springframework.boot:spring-boot-starter-test")
 	}
-
-	/*tasks.getByName("bootJar") {
-		enabled = false
-	}
-
-	tasks.getByName("jar") {
-		enabled = true
-	}*/
 
 	tasks.withType<KotlinCompile> {
 		kotlinOptions {
