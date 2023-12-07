7d8a901bd667993c0bd949e4d2a133b18e6a2c8e
2oublePeace <vova.emiryan@yandex.ru>
2023-12-07 13:04:48 +0400

    Добавлена трассировка вызовов в ReportController

--- report-service/src/main/kotlin/com/emiryanvl/reportservice/controller/ReportController.kt
+++ report-service/src/main/kotlin/com/emiryanvl/reportservice/controller/ReportController.kt
@@ -1,7 +1,10 @@
 package com.emiryanvl.reportservice.controller
 
+import com.emiryanvl.reportservice.config.MdcConfig.Companion.getReportTraceId
 import com.emiryanvl.reportservice.config.ReportProperties
 import com.emiryanvl.reportservice.service.CsvService
+import org.slf4j.LoggerFactory
+import org.slf4j.MDC
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,9 +14,15 @@ import org.springframework.web.client.RestTemplate
 @RestController
 @RequestMapping("/report")
 class ReportController(val reportProperties: ReportProperties, val csvService: CsvService) {
-    @GetMapping
+
+    private val logger = LoggerFactory.getLogger(ReportController::class.java)
+
+    @GetMapping   // Реализуйте ваш способ генерации уникального идентификатора
+        // Например, можно использовать UUID
     fun getReport(): ResponseEntity<String> {
         val disks = RestTemplate().getForObject(reportProperties.reportUrl, String::class.java).toString()
-        return csvService.uploadCsvFile(reportProperties.storageUrl, csvService.convertJsonToCsv(disks))
+        val response = csvService.uploadCsvFile(reportProperties.storageUrl, csvService.convertJsonToCsv(disks))
+        logger.info("Report got with status code {}. TraceId: {}", response.statusCode, MDC.get(getReportTraceId()))
+        return response
     }
 }
